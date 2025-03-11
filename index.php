b2296115a5ec7d3bd439ceb6ca27d7cc9c63bb30
GabrielFalsetti <166294857+GabrielFalsetti@users.noreply.github.com>
2025-03-11 19:47:55 -0300

    Add files via upload
    Inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+class Produto{
+    public $Id;
+    public $Nome;
+    public $Preco;
+} 
+$produto1 = new Produto();
+$Id = 1;
+$Nome = "MacBook ";
+$Preco = 8000;
+
+echo "Produto do ID: " .$Id . " Com o Nome: ".$Nome. "com o preco: ".$Preco;
+
+?>
